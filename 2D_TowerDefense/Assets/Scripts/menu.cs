6dc61d91dcb5a35b5e7c8ef1616debe772493374
cwmichea <cwmichea@uc.cl>
2022-05-18 00:20:07 -0400

    menu to go to the game

--- 2D_TowerDefense/Assets/Scripts/menu.cs
+++ 2D_TowerDefense/Assets/Scripts/menu.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
+    public GameObject settingMenu;
     public void GoToTheGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    public void GoToTheMenu() 
+    {
+        Debug.Log("ESTA WEAAAAAAAAAAAAAAAAAAA");
+        settingMenu.SetActive(true);
+    }
+    public void GoBackToTheMainMenu()
+    {
+        settingMenu.SetActive(false);
     }
 }
